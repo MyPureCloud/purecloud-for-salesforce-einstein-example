8e7f195edc0c4d0ac4b30a545a4aeb5d6e44cdd7
briandavidspencer <brian.spencer@genesys.com>
2023-05-26 12:22:10 -0700

    Removed next best action code

--- src/PureCloudKnowledgeUtilityHelper.js
+++ src/PureCloudKnowledgeUtilityHelper.js
@@ -60,23 +60,10 @@
         component.set('v.numResults', 0);
     },
 
-    updateNextBestAction : function(component, utterance) {
-        var action = component.get("c.updateLastUtterance");
-        action.setParams({'recordId': component.get("v.recordId"), 'utterance': utterance});
-        action.setCallback(this, function(response) {
-            var appEvt = $A.get("e.lightning:nextBestActionsRefresh");
-            appEvt.setParam("recordId", component.get("v.recordId"));
-            appEvt.fire();
-        });
-        $A.enqueueAction(action);
-    },
-    
     handleConversationTranscription : function(component, eventData) {
         if (eventData && eventData.data.transcripts) {
             var x = eventData.data.transcripts.length - 1;
             if (eventData.data.transcripts[x].channel == "EXTERNAL") {
-                //UNCOMMENT LINE BELOW TO ENABLE EINSTEIN NEXT BEST ACTION INTEGRATION
-                //this.updateNextBestAction(component, eventData.data.transcripts[x].alternatives[0].transcript);
                 this.getKnowledgeProbabilities(component, eventData.data.transcripts[x].alternatives[0].transcript);
             }
         }
@@ -86,8 +73,6 @@
         if (eventData && eventData.data.messages) {
             var x = eventData.data.messages.length - 1;
             if (eventData.data.messages[x].role == "customer") {
-                //UNCOMMENT LINE BELOW TO ENABLE EINSTEIN NEXT BEST ACTION INTEGRATION
-				//this.updateNextBestAction(component, eventData.data.messages[x].body);
                 this.getKnowledgeProbabilities(component, eventData.data.messages[x].body);
             }
         }
